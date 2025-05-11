45159333eaa3cf3e3c7f715b81f60c86b486dff5
saragarcia6123 <saragarcia6123@gmail.com>
2025-05-11 04:34:09 +0000

    fix uvicorn port

--- backend/run-uvicorn.sh
+++ backend/run-uvicorn.sh
@@ -1,2 +1,2 @@
 source .venv/bin/activate
-nohup gunicorn -w 4 -k uvicorn.workers.UvicornWorker src.app:app --bind 0.0.0.0:8080 > uvicorn.log 2>&1 &
+nohup gunicorn -w 4 -k uvicorn.workers.UvicornWorker src.app:app --bind 127.0.0.1:8000 > uvicorn.log 2>&1 &
